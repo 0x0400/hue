bed1ccc9aade2a5cdc1c29c4700d160a14f730dd
Romain Rigaux <romain@cloudera.com>
2018-12-14 15:05:09 -0800

    debug qporfile

--- apps/impala/src/impala/api.py
+++ apps/impala/src/impala/api.py
@@ -136,14 +136,17 @@ def alanize(request):
     server_url = _get_impala_server_url(session)
 
   if query_id:
-    LOG.debug("Attempting to get Impala query profile at server_url %s for query ID: %s" % (server_url, query_id))
+    LOG.info("Attempting to get Impala query profile at server_url %s for query ID: %s" % (server_url, query_id))
     doc = Document2.objects.get(id=query_id)
     snippets = doc.data_dict.get('snippets', [])
     secret = snippets[0]['result']['handle']['secret']
     api = get_impalad_api(user=request.user, url=server_url)
     impala_query_id = "%x:%x" % struct.unpack(b"QQ", base64.decodestring(secret))
+
     api.kill(impala_query_id) # There are many statistics that are not present when the query is open. Close it first.
     query_profile = api.get_query_profile_encoded(impala_query_id)
+    LOG.info('QPorifle:')
+    LOG.info(query_profile)
     profile = analyzer.analyze(analyzer.parse_data(query_profile))
     result = ANALYZER.run(profile)
 
